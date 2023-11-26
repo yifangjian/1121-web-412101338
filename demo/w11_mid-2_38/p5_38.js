e4bd6717cfe49d75f5b72cb7f8b5d9ecbb635590
yifangjian <yifangjian015@gmail.com>
2023-11-26 17:23:22 +0800

    W11-P2: convert css of w1-intro and w3-card into scss, and insert them into p5_38.scss

--- demo/w11_mid-2_38/p5_38.js
+++ demo/w11_mid-2_38/p5_38.js
@@ -2,9 +2,9 @@ const navbar = document.querySelector('.nav');
 console.log('navbar', navbar);
 
 window.addEventListener('scroll', function(){
-    if( this.window.pageYOffset > 100){
+    if( this.window.scrollY > 100){
        navbar.classList.add('navbar-fixed');
     } else {
        navbar.classList.remove('navbar-fixed');
     }
-});
+})
