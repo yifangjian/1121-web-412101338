f90bb8d7e97e40993e84fa95f4039fa95718eec8
yifangjian <yifangjian015@gmail.com>
2024-01-08 22:21:25 +0800

    connect

--- demo/js/classdemo.js
+++ demo/js/classdemo.js
@@ -18,7 +18,25 @@ function showdemo(week) {
         p.innerHTML = `<iframe src='../demo/w06_38_blog/index.html' width="100%" height="100%" />`;
         break;
       case 7:
-        p.innerHTML = `<iframe src='../demo/index.html' width="100%" height="100%" />`;
+        p.innerHTML = `<iframe src='../demo/w07_38_showdemo/index.html' width="100%" height="100%" />`;
+        break;
+      case 8:
+        p.innerHTML = `<iframe src='../demo/w08_38_landing/index.html' width="100%" height="100%" />`;
+        break;
+      case 11:
+        p.innerHTML = `<iframe src='../demo/w11_mid-2_38/p5_38.html' width="100%" height="100%" />`;
+        break;
+      case 13:
+        p.innerHTML = `<iframe src='../demo/w13_portfolio_38/index.html' width="100%" height="100%" />`;
+        break;
+      case 14:
+        p.innerHTML = `<iframe src='../demo/w14_38_mega_menu_38/index.html' width="100%" height="100%" />`;
+        break;
+      case 15:
+        p.innerHTML = `<iframe src='../demo/w15_portfolio_38/index.html' width="100%" height="100%" />`;
+        break;
+      case 16:
+        p.innerHTML = `<iframe src='../demo/w16_portfolio_38/index.html' width="100%" height="100%" />`;
         break;
     }
   }
