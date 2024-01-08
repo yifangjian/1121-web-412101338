cfcb1b7f663db0bc327cc7f58dcf80a9f202f753
yifangjian <yifangjian015@gmail.com>
2024-01-08 21:32:46 +0800

    test

---
+++ js/final_38.js
@@ -0,0 +1,33 @@
+const navbar = document.querySelector('#nav');
+const date = document.querySelector('#date');
+
+const navBtn = document.querySelector('#nav-btn');
+const sidebar = document.querySelector('#sidebar');
+const closeBtn = document.querySelector('#close-btn');
+
+// add fixed class to navbar
+window.addEventListener('scroll', function () {
+  if (window.scrollY > 80) {
+    navbar.classList.add('navbar-fixed');
+  } else {
+    navbar.classList.remove('navbar-fixed');
+  }
+});
+
+navBtn.addEventListener('click', function () {
+  sidebar.classList.add('show-sidebar');
+});
+
+closeBtn.addEventListener('click', function () {
+  sidebar.classList.remove('show-sidebar');
+});
+
+// set year
+date.innerHTML = new Date().getFullYear();
+
+//more跳轉
+function scrollToTarget() {
+    document.getElementById('target-section').scrollIntoView({
+      behavior: 'smooth' 
+    });
+  }
